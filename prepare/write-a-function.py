902aeb565e2cab9e7726020de67f8750745953b3
Thiago Ingo Klock <43356541+TinKlo@users.noreply.github.com>
2021-11-03 19:55:24 -0300

    still introductions

---
+++ prepare/write-a-function.py
@@ -0,0 +1,11 @@
+def is_leap(year):
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+
+year = int(input())
+print(is_leap(year))
+
+# Task
+
+# Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+
+# Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function
